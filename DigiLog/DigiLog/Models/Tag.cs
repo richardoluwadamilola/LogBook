d07cf5aef6c44175e3712c99264076ca61ccbf1c
Richard Oluwadamilola <oluwatobyrichard@gmail.com>
2024-01-12 17:30:54 +0100

    updating DepartureTime

--- DigiLog/DigiLog/Models/Tag.cs
+++ DigiLog/DigiLog/Models/Tag.cs
@@ -8,7 +8,7 @@ namespace DigiLog.Models
         [Key]
         [Column(TypeName = "varchar(10)")]
         public string TagNumber { get; set; }
-        public bool IsAvailable { get; set; }
+        public bool IsAvailable { get; set; } = true;
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
         public bool Deleted { get; set; }
--- DigiLog/DigiLog/Services/Implementation/TagService.cs
+++ DigiLog/DigiLog/Services/Implementation/TagService.cs
@@ -3,19 +3,18 @@ using DigiLog.DTOs;
 using DigiLog.Models;
 using DigiLog.Models.ResponseModels;
 using DigiLog.Services.Abstraction;
-using System.Data.SqlTypes;
 
 namespace DigiLog.Services.Implementation
 {
     public class TagService : ITagService
     {
         private readonly LogDbContext _context;
-        
+
 
         public TagService(LogDbContext context)
         {
             _context = context;
-            
+
         }
 
         public ServiceResponse<string> CreateTag(TagDTO tagDto)
@@ -27,7 +26,7 @@ namespace DigiLog.Services.Implementation
                 DateCreated = DateTime.Now,
                 DateModified = DateTime.Now,
                 Deleted = false
-                
+
             };
 
             _context.Tags.Add(tag);
@@ -40,31 +39,42 @@ namespace DigiLog.Services.Implementation
             var response = new ServiceResponse<string>();
 
             // Find an available tag
-            var tag = _context.Tags.FirstOrDefault(t => t.TagNumber == tagNumber && !_context.Visitors.Any(v => v.TagNumber == t.TagNumber));
+            var tag = _context.Tags.FirstOrDefault(t => t.TagNumber == tagNumber);
 
             if (tag == null)
             {
                 response.HasError = true;
-                response.Description = $"Tag with ID {tagNumber} not found or already in use.";
+                response.Description = $"Tag with ID {tagNumber} not found";
                 return response;
             }
 
-            // Assign tag to visitor
-            var visitor = _context.Visitors.Find(visitorId);
-            if (visitor == null)
+
+            if (tag.IsAvailable)
+            {
+                var visitor = _context.Visitors.Find(visitorId);
+                if (visitor == null)
+                {
+                    response.HasError = true;
+                    response.Description = $"Visitor with ID {visitorId} not found.";
+                    return response;
+
+                }
+                visitor.TagNumber = tag.TagNumber;
+                tag.IsAvailable = false;
+                _context.SaveChanges();
+
+                response.HasError = false;
+                response.Description = $"Tag with ID {tagNumber} assigned to Visitor with ID {visitorId} successfully.";
+
+                return response;
+            }
+            else
             {
                 response.HasError = true;
-                response.Description = $"Visitor with ID {visitorId} not found.";
+                response.Description = $"Tag with ID {tagNumber} is not available.";
                 return response;
             }
 
-            visitor.TagNumber = tag.TagNumber;
-            _context.SaveChanges();
-
-            response.HasError = false;
-            response.Description = $"Tag with ID {tagNumber} assigned to Visitor with ID {visitorId} successfully.";
-
-            return response;
         }
 
         public ServiceResponse<string> CheckOutVisitor(string tagNumber)
@@ -81,16 +91,25 @@ namespace DigiLog.Services.Implementation
                 return response;
             }
 
+            // Check if the visitor is already checked out
+            if (visitor.DepartureTime > DateTime.MinValue)
+            {
+                response.HasError = true;
+                response.Description = $"Visitor with Tag ID {tagNumber} is already checked out.";
+                return response;
+            }
+
             // Set departure time to the current time
             visitor.DepartureTime = DateTime.Now;
 
-            //Reassign tag 
+            // Reassign tag 
             var tag = _context.Tags.FirstOrDefault(t => t.TagNumber == tagNumber);
 
             if (tag != null)
             {
                 tag.IsAvailable = true;
             }
+
             _context.SaveChanges();
 
             response.HasError = false;
@@ -99,16 +118,17 @@ namespace DigiLog.Services.Implementation
             return response;
         }
 
-        public List<TagDTO> GetTags()
-        {
-            return _context.Tags
-                .Select(tag => new TagDTO
-                {
-                    TagNumber = tag.TagNumber
-                })
-                .ToList();
-        }
 
-        
+        public List<TagDTO> GetTags()
+    {
+        return _context.Tags
+            .Select(tag => new TagDTO
+            {
+                TagNumber = tag.TagNumber
+            })
+            .ToList();
     }
+
+
+}
 }
